0544683c444351e0413e4a0986c8268264ae7a1c
Nassos Katsamanis <nassos@n12mavra.cs.ntua.gr>
2014-02-03 15:26:43 +0200

    fixing paths

--- prepare_beam.sh
+++ prepare_beam.sh
@@ -1,10 +1,10 @@
 #mkdir -p audio
 #mkdir -p lab
-lab_dir=/rmt/work/speech_activity_detection_git/lab/transcr_beam/dev1
+lab_dir=/rmt/work/speech_activity_detection/lab/transcr_beam/dev1
 training_script=lists/train.script_beam
 testing_script=lists/test.script_beam
 
-#rm -f $training_script $testing_script
+rm -f $training_script $testing_script
 for i in `ls /rmt5/databases/DIRHA/DIRHA_simcorpora2_v3/GR/dev1/simulations/sim*/Signals/Mixed_Sources/*/*/*.wav | grep "beam_on_sp_comm_read"`; 
 do 
     sim_id=`echo $i | grep -o 'sim[[:digit:]]\+'`;
@@ -15,13 +15,13 @@ do
     new_lab_file_name=lab/${new_id}.lab
     
  
-    ln -s $i $new_audio_file_name;
+    ln -sf $i $new_audio_file_name;
  #  # ln -s $lab_dir/${mic_id}/${mic_id}`echo $sim_id | sed s/sim//`.lab $new_lab_file_name; 
-    ln -s $lab_dir/dev1_${sim_id}_${room_id}.lab $new_lab_file_name;
+    ln -sf $lab_dir/dev1_${sim_id}_${room_id}.lab $new_lab_file_name;
     echo $new_audio_file_name $new_lab_file_name >> $training_script;
 done;
 
-lab_dir=/rmt/work/speech_activity_detection_git/lab/transcr_beam/test1
+lab_dir=/rmt/work/speech_activity_detection/lab/transcr_beam/test1
 for i in `ls /rmt5/databases/DIRHA/DIRHA_simcorpora2_v3/GR/test1/simulations/sim*/Signals/Mixed_Sources/*/*/*.wav | grep "beam_on_sp_comm_read"`; 
 do 
     sim_id=`echo $i | grep -o 'sim[[:digit:]]\+'`;
@@ -31,9 +31,9 @@ do
     new_audio_file_name=audio/${new_id}.wav
     new_lab_file_name=lab/${new_id}.lab
 
-    ln -s $i $new_audio_file_name;
+    ln -sf $i $new_audio_file_name;
    # ln -s $lab_dir/${mic_id}/${mic_id}`echo $sim_id | sed s/sim//`.lab $new_lab_file_name;
-    ln -s $lab_dir/test1_${sim_id}_${room_id}.lab $new_lab_file_name; 
+    ln -sf $lab_dir/test1_${sim_id}_${room_id}.lab $new_lab_file_name; 
     echo $new_audio_file_name $new_lab_file_name >> $testing_script;
 
   #  echo "something found..";
