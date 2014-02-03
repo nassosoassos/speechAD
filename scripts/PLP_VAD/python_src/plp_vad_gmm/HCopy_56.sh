543f904d9d0c63e4341204eaee37322ff4c02c51
Nassos Katsamanis <nassos@n12mavra.cs.ntua.gr>
2014-02-03 15:25:53 +0200

    cleaning up unnecessary files

--- scripts/PLP_VAD/python_src/plp_vad_gmm/HCopy_56.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#$ -S /bin/bash
-#$ -cwd
-
-/home/nassos/bin/HCopy -S /rmt/work/speech_activity_detection/experiments/dirha_gmm/MFCC_D_A_Z_0/data_56.scp -C /rmt/work/speech_activity_detection/experiments/dirha_gmm/MFCC_D_A_Z_0/hcopy_config
